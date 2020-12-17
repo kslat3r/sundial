28bad20d2a71d1a18b44a66fcd9d96f1956d7202
Ed Kelly <me@edkelly.co.uk>
2020-12-17 17:26:51 +0000

    Monorepo CircleCI

--- app/recipe-service/helm/install.sh
+++ app/recipe-service/helm/install.sh
@@ -2,5 +2,5 @@
 
 helm upgrade --install breakfast-service -f breakfast-service/values.yaml --set image.tag=$CIRCLE_SHA1 recipe-service
 helm upgrade --install lunch-service -f lunch-service/values.yaml --set image.tag=$CIRCLE_SHA1 recipe-service
-helm upgrade --install dinner-service -f dinner-servicevalues.yaml --set image.tag=$CIRCLE_SHA1 recipe-service
+helm upgrade --install dinner-service -f dinner-service/values.yaml --set image.tag=$CIRCLE_SHA1 recipe-service
 
