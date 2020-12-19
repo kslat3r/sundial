4c50ae4b40e54326536c0898fde01aab47a1035b
Ed Kelly <me@edkelly.co.uk>
2020-12-19 22:37:13 +0000

    Monorepo CircleCI

--- infrastructure/k8s/apply.sh
+++ infrastructure/k8s/apply.sh
@@ -6,4 +6,7 @@ kubectl create ns api || true
 kubectl create ns istio-system || true
 kubectl create ns sundial-system || true
 
+kubectl label namespace api istio-injection=enabled --overwrite
+kubectl label namespace default sundial-system istio-injection=disabled --overwrite
+
 kubectl apply -f metrics-server.yml
--- infrastructure/tf/main.tf
+++ infrastructure/tf/main.tf
@@ -1,11 +1,11 @@
 resource "digitalocean_kubernetes_cluster" "sundial-cluster" {
-  name    = "sundial-cluster"
-  region  = "lon1"
+  name = "sundial-cluster"
+  region = "lon1"
   version = "1.19.3-do.2"
 
   node_pool {
-    name       = "sundial-pool"
-    size       = "s-2vcpu-4gb"
+    name = "sundial-pool"
+    size = "s-2vcpu-4gb"
     auto_scale = false
     node_count = 3
   }
