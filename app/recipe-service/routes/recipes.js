4afa4fe1da4174c7aa6e373b501c8ba16463dcd3
Ed Kelly <me@edkelly.co.uk>
2020-12-19 14:52:23 +0000

    Monorepo CircleCI

--- app/recipe-service/routes/recipes.js
+++ app/recipe-service/routes/recipes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const fibonacci = require('fibonacci')
+// const fibonacci = require('fibonacci')
 const data = require('../data/recipes.json')
 
 router.get(`/recipes`, async (req, res) => {
-  const fib = fibonacci.iterate(process.env.FIBONACCI ? parseInt(process.env.FIBONACCI) : 5000)
+  // const fib = fibonacci.iterate(process.env.FIBONACCI ? parseInt(process.env.FIBONACCI) : 5000)
 
-  data[0].number = fib.number
+  // data[0].number = fib.number
 
   res.send(data)
 })
