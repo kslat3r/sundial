7b2558007c1fef238a1f08395643cb5440b8f7d7
Ed Kelly <me@edkelly.co.uk>
2020-12-20 14:29:13 +0000

    Monorepo CircleCI

--- app/training-data-generator/helm/install.sh
+++ app/training-data-generator/helm/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-helm upgrade --install 1s-training-data-generator \
+helm upgrade --install training-data-generator \
   -f training-data-generator/values.yaml \
   --set image.tag=$CIRCLE_SHA1 \
   --set step=1s \
   --set dataFile=1straining.csv \
   1s-training-data-generator
 
-helm upgrade --install 30s-training-data-generator \
+helm upgrade --install training-data-generator \
   -f training-data-generator/values.yaml \
   --set image.tag=$CIRCLE_SHA1 \
   --set step=30s \
