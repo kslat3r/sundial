4977bd58975e3a4ffe119ac6e05a0402d13ffe6a
Ed Kelly <me@edkelly.co.uk>
2020-12-20 13:19:33 +0000

    Monorepo CircleCI

--- app/training-data-generator/queries/avg-container-memory-usage.js
+++ app/training-data-generator/queries/avg-container-memory-usage.js
@@ -1,3 +1,3 @@
 const range = require('./range');
 
-module.exports = (interval, start, ns) => range(`avg(rate(container_memory_usage_bytes{namespace="${ns}", container_name=~".*-service"}[${interval}])) by (container_name)`, start);
+module.exports = (interval, start, ns) => range(`avg(rate(container_memory_usage_bytes{namespace="${ns}", container=~".*-service"}[${interval}])) by (container)`, start);
